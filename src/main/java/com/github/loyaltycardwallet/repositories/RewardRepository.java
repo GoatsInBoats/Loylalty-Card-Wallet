98af240c8a9cd996d1dc2212c4ae5e1d8b073343
byrtus <byrtus@wp.pl>
2020-06-02 19:10:25 +0200

    Create RewardRepository interface

---
+++ src/main/java/com/github/loyaltycardwallet/repositories/RewardRepository.java
@@ -0,0 +1,12 @@
+package com.github.loyaltycardwallet.repositories;
+
+import com.github.loyaltycardwallet.models.Reward;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface RewardRepository extends JpaRepository<Reward, UUID> {
+    boolean existsById(UUID id);
+}
