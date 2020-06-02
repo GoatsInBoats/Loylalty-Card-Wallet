4015f84c81217fcd14bd6dc2d07af9c440454fd6
AdamPietras <adam.marek.pietras@gmail.com>
2020-06-02 18:26:06 +0200

    Implement and override methods from UserService. Implemented UserDetailsService interface from springsecurity, which has one method important for authentication

---
+++ src/main/java/com/github/loyaltycardwallet/services/implementation/UserServiceImpl.java
@@ -0,0 +1,56 @@
+package com.github.loyaltycardwallet.services.implementation;
+
+import com.github.loyaltycardwallet.models.User;
+import com.github.loyaltycardwallet.repositories.UserRepository;
+import com.github.loyaltycardwallet.services.UserService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class UserServiceImpl implements UserService, UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> findById(UUID id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public boolean existById(UUID id) {
+        return userRepository.existsById(id);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return findByUsername(username);
+    }
+}
