8ae688cf32c76a5bb6e05e55985c8b01971b27d2
AdamPietras <adam.marek.pietras@gmail.com>
2020-06-02 10:33:29 +0200

    inital commit

---
+++ src/main/java/com/github/loyaltycardwallet/LoyaltyCardWalletApplication.java
@@ -0,0 +1,13 @@
+package com.github.loyaltycardwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyaltyCardWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoyaltyCardWalletApplication.class, args);
+	}
+
+}
