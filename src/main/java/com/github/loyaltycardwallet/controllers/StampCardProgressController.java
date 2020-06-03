73f3ef9c051a8adc4e6ddd7d88f3c835e20688f0
byrtus <byrtus@wp.pl>
2020-06-03 13:42:33 +0200

    Create controller for StampCardProgress & add delete, edit, add functionality

---
+++ src/main/java/com/github/loyaltycardwallet/controllers/StampCardProgressController.java
@@ -0,0 +1,64 @@
+package com.github.loyaltycardwallet.controllers;
+
+import com.github.loyaltycardwallet.models.StampCardProgress;
+import com.github.loyaltycardwallet.services.StampCardProgressService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/stampcards-progresses")
+public class StampCardProgressController {
+    private StampCardProgressService stampCardProgressService;
+
+    public StampCardProgressController(StampCardProgressService stampCardProgressService) {
+        this.stampCardProgressService = stampCardProgressService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<StampCardProgress>> getAll() {
+        return ResponseEntity.ok(stampCardProgressService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<StampCardProgress> getById(@PathVariable UUID id) {
+        return stampCardProgressService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<StampCardProgress> add(@RequestBody @Valid StampCardProgress stampCardProgress) {
+        StampCardProgress newStampCardProgress = stampCardProgressService.save(stampCardProgress);
+        return ResponseEntity.created(URI.create("/" + newStampCardProgress.getId())).body(newStampCardProgress);
+    }
+
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<StampCardProgress> updateUser(@PathVariable UUID id, @RequestBody @Valid StampCardProgress stampCardProgress) {
+        if (!stampCardProgressService.existById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        stampCardProgress.setId(id);
+        stampCardProgressService.save(stampCardProgress);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteById(@PathVariable UUID id) {
+        stampCardProgressService.deleteById(id);
+        if (!stampCardProgressService.existById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.noContent().build();
+    }
+
+}
